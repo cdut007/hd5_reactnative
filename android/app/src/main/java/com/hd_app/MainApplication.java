bbde479f4dee8da7af41c4a86512bf766d4fd489
lrobot <lrobot.qq@gmail.com>
2017-10-19 20:25:51 +0800

    update

--- android/app/src/main/java/com/hd_app/MainApplication.java
+++ android/app/src/main/java/com/hd_app/MainApplication.java
@@ -16,6 +16,7 @@ import com.hd_app.log.LogReactPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactlibrary.RNCardViewPackage;
 import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 
 import java.util.Arrays;
 import java.util.List;
@@ -50,7 +51,9 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
+        Beta.initDelay = 5 * 1000;
         Bugly.init(getApplicationContext(), "8627b7cc8e", false);
+
         final String appName = this.getString(R.string.app_name);
         final String logPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + appName + "/log";
 
